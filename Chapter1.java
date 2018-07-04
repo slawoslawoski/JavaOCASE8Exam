035207c42a0f36c76737c7517f153a5d74e82de7
slawoslawoski <slawomir.opoka@gmail.com>
2018-07-04 15:57:24 +0200

    zadanie 1.1

--- Chapter1.java
+++ Chapter1.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class Chapter1 {
     public static void main(String[] args) {
-Random r = new Random();
-System.out.println(r.nextInt(10));
+Random r = new Random(); 
+System.out.println(r.nextInt(77));
 
 
     }
